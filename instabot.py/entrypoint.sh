55f3b557afc70b9d98fa2f5aee2316f720103113
lrobin <uuaing@gmail.com>
2017-06-22 19:27:09 +0800

    ff

--- instabot.py/entrypoint.sh
+++ instabot.py/entrypoint.sh
@@ -3,11 +3,11 @@
 sed -i -r 's/(login *=).*/\1os.environ.get("INS_NAME"),/g' example.py
 sed -i -r 's/(password *=).*/\1os.environ.get("INS_PASSWD"),/g' example.py
 sed -i -r 's/(tag_list *=).*/\1"[" + os.environ.get("TAGS") + "]",/g' example.py
-sed -i -r 's/(like_per_day *=).*/\1os.environ.get("LIKE_PER_DAY"),/g' example.py
-sed -i -r 's/(comments_per_day *=).*/\1os.environ.get("COMMENTS_PER_DAY"),/g' example.py
-sed -i -r 's/(follow_per_day *=).*/\1os.environ.get("FOLLOW_PER_DAY"),/g' example.py
-sed -i -r 's/(unfollow_per_day *=).*/\1os.environ.get("UNFOLLOW_PER_DAY"),/g' example.py
-sed -i -r 's/(log_mod *=).*/\1os.environ.get("LOG_MODE"),/g' example.py
+sed -i -r 's/(like_per_day *=).*/\1int(os.environ.get("LIKE_PER_DAY")),/g' example.py
+sed -i -r 's/(comments_per_day *=).*/\1int(os.environ.get("COMMENTS_PER_DAY")),/g' example.py
+sed -i -r 's/(follow_per_day *=).*/\1int(os.environ.get("FOLLOW_PER_DAY")),/g' example.py
+sed -i -r 's/(unfollow_per_day *=).*/\1int(os.environ.get("UNFOLLOW_PER_DAY")),/g' example.py
+sed -i -r 's/(log_mod *=).*/\1int(os.environ.get("LOG_MODE")),/g' example.py
 
 if [ "$PROXY" != "" ]; then 
   sed -i -r 's/(proxy *=).*/\1os.environ.get("PROXY"),/g' example.py
