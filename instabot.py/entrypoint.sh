bf2e5f80f42192946d2debf23517760ac736e14b
lrobin <uuaing@gmail.com>
2017-06-22 15:35:08 +0800

    update

--- instabot.py/entrypoint.sh
+++ instabot.py/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sed -i -r 's/(login *=).*/\1os.environ.get("INS_NAME")/g' example.py
-sed -i -r 's/(password *=).*/\1os.environ.get("INS_PASSWD")/g' example.py
+sed -i -r 's/(login *=).*/\1os.environ.get("INS_NAME"),/g' example.py
+sed -i -r 's/(password *=).*/\1os.environ.get("INS_PASSWD"),/g' example.py
 
 if [ "$PROXY" != "" ]; then 
-  sed -i -r 's/(proxy *=).*/\1os.environ.get("PROXY")/g' example.py
+  sed -i -r 's/(proxy *=).*/\1os.environ.get("PROXY"),/g' example.py
 fi
 
 python example.py
