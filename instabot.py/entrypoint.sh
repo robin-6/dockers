6e5b722d138b0e44d41dce45f02a04705c4c4f86
lrobin <uuaing@gmail.com>
2017-06-23 10:55:14 +0800

    fix tag_list

--- instabot.py/entrypoint.sh
+++ instabot.py/entrypoint.sh
@@ -2,7 +2,7 @@
 
 sed -i -r 's/(login *=).*/\1os.environ.get("INS_NAME"),/g' example.py
 sed -i -r 's/(password *=).*/\1os.environ.get("INS_PASSWD"),/g' example.py
-sed -i -r 's/(tag_list *=).*/\1"[" + os.environ.get("TAGS") + "]",/g' example.py
+sed -i -r 's/(tag_list *=).*/\1eval("[" + os.environ.get("TAGS") + "]"),/g' example.py
 sed -i -r 's/(like_per_day *=).*/\1int(os.environ.get("LIKE_PER_DAY")),/g' example.py
 sed -i -r 's/(comments_per_day *=).*/\1int(os.environ.get("COMMENTS_PER_DAY")),/g' example.py
 sed -i -r 's/(follow_per_day *=).*/\1int(os.environ.get("FOLLOW_PER_DAY")),/g' example.py
